bbc1cfe5305078946c547bae1e19610e708e60b3
MuhammadRizwan-29 <engrrizwanjamali@gmail.com>
2025-03-04 23:24:26 +0500

    Minor changes in these files App.jsx, Navbar.jsx & StoreContext.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Menu from "./pages/Menu/Menu";
 import Footer from "./components/Footer/Footer";
+import { useState } from "react";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import "./Navbar.css";
 import { assets } from "./../../assets/assets";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router";
+import { StoreContext } from "../../context/StoreContext";
 
-export default function Navbar() {
+export default function Navbar({ setShowLogin }) {
   const [menu, setMenu] = useState("home");
+  const { getTotalCartAmount } = useContext(StoreContext);
   return (
     <header className="navbar">
-      <img src={assets.logo} alt="logo" className="logo" />
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="logo" />
+      </Link>
       <div className="navbar_menu">
         <ul>
           <Link
@@ -43,10 +47,12 @@ export default function Navbar() {
       <div className="navbar_right">
         <img src={assets.search_icon} alt="search" />
         <div className="navbar_search_icon">
-          <img src={assets.basket_icon} alt="cart" />
-          <div className="dot"></div>
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="cart" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
-        <button>sign in</button>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </header>
   );
